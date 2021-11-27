839aee12a80b65c59cd6c01a719c87f165af264a
Никита Глушаченко <rylonoboev2004@gmail.com>
2021-11-27 19:10:56 +0300

    Del

--- src/com/JavaGame/Frame.java
+++ src/com/JavaGame/Frame.java
@@ -11,33 +11,3 @@ import java.awt.*;
  * FOR C.T.S., DOCENT NESTERENKOV S.N.
  **/
 
-public class Frame extends JFrame {
-    private static Frame instanceFrame = new Frame();
-
-    private static final long serialVersionUID = 1L;/**Delete**/
-
-    public Frame() {
-        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        setTitle("Game Snake");
-        setResizable(false);
-
-        init();
-    }
-
-    public void init() {
-        setLayout(new GridLayout(1, 1, 0, 0));/**Delete**/
-        GameScreen Screen = new GameScreen();
-        add(Screen);
-        pack();
-        setLocationRelativeTo(null);
-        setVisible(true);
-        //WordsLabel.getInstance().updateText("Snake Game");
-    }
-
-    public static void main(String[] args) {
-
-        new Frame();/**Delete**/
-    }
-    public static Frame getInstance() { return instanceFrame; }
-
-}
