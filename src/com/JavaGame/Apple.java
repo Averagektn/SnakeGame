ee0fccce12f9df71e58c22e12153813979df374e
Никита Глушаченко <rylonoboev2004@gmail.com>
2021-11-27 19:13:58 +0300

    Update Apple.java

--- src/com/JavaGame/Apple.java
+++ src/com/JavaGame/Apple.java
@@ -5,3 +5,13 @@ import java.awt.Color;
 
 import java.awt.*;
 
+public class Apple {
+
+    private int xCoor, yCoor, width, height;
+
+    public Apple(int xCoor, int yCoor, int tileSize) {
+        this.xCoor = xCoor;
+        this.yCoor = yCoor;
+        width = tileSize;
+        height = tileSize;
+    }
