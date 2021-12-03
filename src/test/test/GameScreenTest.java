9616b58a5f54d5213a748f211a5c4adbb6516d43
Никита Глушаченко <rylonoboev2004@gmail.com>
2021-12-03 23:11:23 +0300

    Revert "Update GameScreenTest.java"
    This reverts commit 8b820f6b3c7f880b0b506bcc30f1fd8cd5397c7f.

---
+++ src/test/test/GameScreenTest.java
@@ -0,0 +1,11 @@
+
+import com.JavaGame.GameScreen;
+import org.junit.Assert;
+import org.junit.Test;
+
+public class GameScreenTest {
+    @Test
+    public void isNotNull() {
+        Assert.assertNotNull(GameScreen.getInstance());
+    }
+}
