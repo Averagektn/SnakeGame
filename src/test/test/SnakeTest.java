b5ec66ae87421cccaa56144fbfb296405f13c4aa
Никита Глушаченко <rylonoboev2004@gmail.com>
2021-12-03 23:11:26 +0300

    Revert "Update SnakeTest.java"
    This reverts commit f61f3a502fb91ecd335bd7b17098ab39b067cfb4.

---
+++ src/test/test/SnakeTest.java
@@ -0,0 +1,11 @@
+
+import com.JavaGame.Snake;
+import org.junit.Assert;
+import org.junit.Test;
+
+public class SnakeTest {
+    @Test
+    public void isNotNull() {
+        Assert.assertNotNull(Snake.getInstanceSnake());
+    }
+}
